b169e55b6c28eaa4939ea4b9db245ef8ca986522
kluckkk <selvyalexandra05@gmail.com>
2024-11-22 17:38:24 +0700

    Update PlayGame.h

--- PlayGame.h
+++ PlayGame.h
@@ -22,36 +22,40 @@ class TFrmPlayLevel : public TForm
 {
 __published:	// IDE-managed Components
 	TImage *ImgScr;
-	TProgressBar *HPBarUser;
-	TProgressBar *ProgressBar1;
+	TProgressBar *UserBar;
+	TProgressBar *MusuhBar;
 	TADOConnection *KoneksiDB;
 	TADOQuery *QueryWord;
 	TLabel *Label1;
 	TADOTable *TblUserWord;
 	TLabel *Label2;
+	TProgressBar *EnergyBar;
 	void __fastcall FormCreate(TObject *Sender);
 	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
 	void __fastcall FormKeyPress(TObject *Sender, System::WideChar &Key);
 
 private:	// User declarations
-	tDisplay RoachImg[3][21];
-	tDisplay NewsImg[10], Letters[2][26];
+	tDisplay RoachImg[4][21];
+	tDisplay NewsImg[2][10], Letters[2][26];
 	char **WordsDB, WordInput[15], WordOutput[15];
 
 	bool WordIsMoving;
 	int PoseNo, lWordInput, WordCount, xWord, yWord, LetterWidth, LetterHeight;
 	int rand, HurufKe;
-	int RoachWidth[3][21], RoachHeight[3][21], SprNo, xRoach, yRoach, TickCount;
+	int NPoseNo, RoachWidth[4][21], RoachHeight[4][21], SprNo, xRoach, yRoach, TickCount;
 	//int RoachAtkW[21], RoachAtkH[21], RASprNo, xRoachAtk, yRoachAtk, RATickCount;
-	int NewsWidth[10], NewsHeight[10], NSprNo, xNews, yNews, NTickCount;
-	bool Hit, Atk, Hurt;
+	int NewsWidth[2][10], NewsHeight[2][10], NSprNo, xNews, yNews, NTickCount;
+	bool Hit, Atk, Hurt, Dead;
     bool FirstCorrect, SecondCorrect, ThirdCorrect;
 
     //(biar key bs diluar keypress)
 	char LastKeyPressed;
 	bool KeyWasPressed;
+    bool Ulti;
 	//bool CekLetter[3] = {false, false, false};
-    int ColorNo[3]={0,0,0};
+	int ColorNo[3]={0,0,0};
+	int HandHP, KecoaHP;
+    int Speed;
 
 	Graphics::TBitmap *BM;
 	TLocateOptions LO;
